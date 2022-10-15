6342446952fef40c78cb261534b768c088d97558
Zobaer <zzobaer09@gmail.com>
2022-10-15 09:25:23 +0600

    forms added

---
+++ first_project/app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class ToDoForm(forms.Form):
+    name = forms.CharField(max_length=255 , label="Name")
+    items = forms.CharField(max_length=255 , label="Item")
+    complete = forms.BooleanField(label="Complete")
