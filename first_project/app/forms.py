f62da91e863608d331c3095cbcb2c59fca12b831
Zobaer <zzobaer09@gmail.com>
2022-10-15 10:30:45 +0600

    class for form added

--- first_project/app/forms.py
+++ first_project/app/forms.py
@@ -5,3 +5,5 @@ class ToDoForm(forms.Form):
     name = forms.CharField(max_length=255 , label="Name")
     items = forms.CharField(max_length=255 , label="Item")
     complete = forms.BooleanField(label="Complete")
+
+    
