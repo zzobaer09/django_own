aaa4c5715a655f6b58424f24ba9c7915047f398d
Zobaer <zzobaer09@gmail.com>
2022-10-17 18:41:53 +0600

    /create/ page is removed .same functionality added to list page.

--- first_project/app/views.py
+++ first_project/app/views.py
@@ -24,7 +24,29 @@ def home(response):
 
 def list(response):
     list_ = [i.name for i in ls.all()]
-    return HttpResponse(render(response , "app/list.html",{"ls":list_}))
+
+    if response.method == "POST":
+        create_form = ToDoForm(response.POST)
+        if create_form.is_valid():
+
+            # all value
+            _name = create_form.cleaned_data["name"]
+            _item = create_form.cleaned_data["items"]
+            _complete = create_form.cleaned_data["complete"]
+
+            """
+                database work
+            """
+
+            t = ToDoList(name=_name)
+            t.save()
+            t.item_set.create(text=_item , complete=_complete)
+            return HttpResponseRedirect("{}/".format(t.name))
+
+    else:
+        create_form = ToDoForm()
+
+    return HttpResponse(render(response , "app/list.html",{"ls": list_ , "FORM": create_form}))
 
 
 def log_in(response):
@@ -68,29 +90,29 @@ def view_data(response , name):
 
 
 
-def create(response):
+# def create(response):
 
-    if response.method == "POST":
-        create_form = ToDoForm(response.POST)
-        if create_form.is_valid():
+#     if response.method == "POST":
+#         create_form = ToDoForm(response.POST)
+#         if create_form.is_valid():
 
-            # all value
-            _name = create_form.cleaned_data["name"]
-            _item = create_form.cleaned_data["items"]
-            _complete = create_form.cleaned_data["complete"]
+#             # all value
+#             _name = create_form.cleaned_data["name"]
+#             _item = create_form.cleaned_data["items"]
+#             _complete = create_form.cleaned_data["complete"]
 
-            """
-                database work
-            """
+#             """
+#                 database work
+#             """
 
-            t = ToDoList(name=_name)
-            t.save()
-            t.item_set.create(text=_item , complete=_complete)
-            return HttpResponseRedirect("../{}/".format(t.name))
+#             t = ToDoList(name=_name)
+#             t.save()
+#             t.item_set.create(text=_item , complete=_complete)
+#             return HttpResponseRedirect("../{}/".format(t.name))
 
-    else:
-        create_form = ToDoForm()
-    return HttpResponse(render(response , "app/create.html" , {"FORM":create_form}))
+#     else:
+#         create_form = ToDoForm()
+#     return HttpResponse(render(response , "app/create.html" , {"FORM":create_form}))
 
 
 
