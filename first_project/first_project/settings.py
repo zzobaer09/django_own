cdadb741766b53236664f19516d39d07b16ed26e
Zobaer <zzobaer09@gmail.com>
2022-10-20 14:56:51 +0600

    "merged from pre_features"

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'app.apps.AppConfig',
     'register.apps.RegisterConfig',
 ]
@@ -119,7 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
